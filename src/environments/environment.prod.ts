5584fed497f1b8ff44e5e2b91fbb47a566f40e25
Mauricio Llumiquinga <oiciruam73@hotmail.com>
2019-11-14 23:47:44 -0500

    Add users authentication with Firebase and add login and register page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  api_url: 'https://platzi-store.herokuapp.com'
+  api_url: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseURL',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://platzi-store.herokuapp.com'
+  api_url: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseURL',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId'
+}
 };
 
 /*
